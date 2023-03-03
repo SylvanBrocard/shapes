565733236947381eb3891ea8947a293766178fe5
SylvanBrocard <sylvan.brocard@gmail.com>
2023-03-03 23:50:54 +0100

    format

--- shapes_benchmark.cpp
+++ shapes_benchmark.cpp
@@ -4,60 +4,72 @@
 
 #include "oopshapes.h"
 
-class OOPShapesFixture : public benchmark::Fixture {
+class OOPShapesFixture : public benchmark::Fixture
+{
 public:
   u32 ShapeCount = 1e7;
   shape_base **Shapes;
 
-  void SetUp(const ::benchmark::State &state) {
+  void SetUp(const ::benchmark::State &state)
+  {
     Shapes = new shape_base *[ShapeCount];
-    timespec* tp;
+    timespec *tp;
     clock_gettime(CLOCK_REALTIME, tp);
     srand(tp->tv_nsec);
-    #pragma omp simd
-    for (u32 ShapeIndex = 0; ShapeIndex < ShapeCount; ++ShapeIndex) {
-        u32 ShapeType = rand() % 4;
-        switch(ShapeType)
-        {
-            case 0:
-            {
-                f32 Side = rand() % 100;
-                Shapes[ShapeIndex] = new square(Side);
-            } break;
-            
-            case 1:
-            {
-                f32 Width = rand() % 100;
-                f32 Height = rand() % 100;
-                Shapes[ShapeIndex] = new rectangle(Width, Height);
-            } break;
-            
-            case 2:
-            {
-                f32 Base = rand() % 100;
-                f32 Height = rand() % 100;
-                Shapes[ShapeIndex] = new triangle(Base, Height);
-            } break;
-            
-            case 3:
-            {
-                f32 Radius = rand() % 100;
-                Shapes[ShapeIndex] = new circle(Radius);
-            } break;
-        }
+#pragma omp simd
+    for (u32 ShapeIndex = 0; ShapeIndex < ShapeCount; ++ShapeIndex)
+    {
+      u32 ShapeType = rand() % 4;
+      switch (ShapeType)
+      {
+      case 0:
+      {
+        f32 Side = rand() % 100;
+        Shapes[ShapeIndex] = new square(Side);
+      }
+      break;
+
+      case 1:
+      {
+        f32 Width = rand() % 100;
+        f32 Height = rand() % 100;
+        Shapes[ShapeIndex] = new rectangle(Width, Height);
+      }
+      break;
+
+      case 2:
+      {
+        f32 Base = rand() % 100;
+        f32 Height = rand() % 100;
+        Shapes[ShapeIndex] = new triangle(Base, Height);
+      }
+      break;
+
+      case 3:
+      {
+        f32 Radius = rand() % 100;
+        Shapes[ShapeIndex] = new circle(Radius);
+      }
+      break;
+      }
     }
   }
 
-  void TearDown(const ::benchmark::State &state) {
-    for (u32 ShapeIndex = 0; ShapeIndex < ShapeCount; ++ShapeIndex) {
+  void TearDown(const ::benchmark::State &state)
+  {
+    for (u32 ShapeIndex = 0; ShapeIndex < ShapeCount; ++ShapeIndex)
+    {
       delete Shapes[ShapeIndex];
     }
     delete[] Shapes;
   }
 };
 
-BENCHMARK_DEFINE_F(OOPShapesFixture, TotalAreaVTBL)(benchmark::State &state) {
-  for (auto _ : state) {
+BENCHMARK_DEFINE_F(OOPShapesFixture, TotalAreaVTBL)
+(benchmark::State &state)
+{
+  for (auto _ : state)
+  {
     auto Accum = TotalAreaVTBL(ShapeCount, Shapes);
     std::cout << Accum << std::endl;
   }
